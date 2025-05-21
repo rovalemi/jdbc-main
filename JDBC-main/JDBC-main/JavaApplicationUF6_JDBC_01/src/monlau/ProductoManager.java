16cf4aacf14340e377e48728502abd373ac20f53
rovalemi <rovaalemi.es@gmail.com>
2025-05-21 11:55:32 +0200

    Update ProductoManager.java

--- JDBC-main/JDBC-main/JavaApplicationUF6_JDBC_01/src/monlau/ProductoManager.java
+++ JDBC-main/JDBC-main/JavaApplicationUF6_JDBC_01/src/monlau/ProductoManager.java
@@ -10,10 +10,42 @@ public class ProductoManager {
         ProductoDAO productoDao =new ProductoDAOImpl();
 
         // Agregar nuevo producto
-//        Producto nuevoProducto = new Producto(3,"Fresa",4.5);
-        Producto nuevoProducto = new Producto("Fresa",4.5);
-        productoDao.insert(nuevoProducto);
+        Producto productoN = new Producto(200,"Manzana",5.5);
+        productoDao.insert(productoN);
+        System.out.println("\nSe ha agregado el producto: " + productoN);
+        
+        // Obtener el producto con el ID = 200
+        Producto productoS = productoDao.read(200);
+        if (productoS != null) {
+            System.out.println("\nSe ha encontrado el producto: " + productoS);
+        } else {
+            System.out.println("\nProducto con ID: 200 no encontrado.");
+        }
 
+        // Actualizar el producto con el ID = 200
+        if (productoS != null) {
+            productoS.setNombre("Pera");
+            productoS.setPrecio(5.5);
+            System.out.println("");
+            productoDao.update(productoS);
+        } else {
+            System.out.println("\nProducto con ID: 200 no encontrado.");
+        }
 
+        // Eliminar el producto
+        if (productoS != null) {
+            System.out.println("");
+            productoDao.delete(productoS);
+        } else {
+            System.out.println("\nProducto con ID: 200 no encontrado.");
+        }
+
+        // Intentar leer de nuevo el producto
+        Producto productoE = productoDao.read(200);
+        if (productoE == null) {
+            System.out.println("\nProducto con ID: 200 no se encuentra (eliminado).");
+        } else {
+            System.out.println("\nError: el producto todavia existe: " + productoE);
+        }
     }
 }
